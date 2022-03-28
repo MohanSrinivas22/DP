c2ee88d525fc6363d77597e81f2eed3bae3d5a15
Mohan22 <81840891+MohanSrinivas22@users.noreply.github.com>
2022-03-28 21:52:07 +0530

    Update howSum(Memo).py

--- howSum(Memo).py
+++ howSum(Memo).py
@@ -8,8 +8,8 @@ def howSum(targetSum, nums, memo):
         if res != None : 
             memo[targetSum] = res + [num]
             return memo[targetSum]
+    memo[targetSum]=None
     return None
-
 
 print(howSum(7, [2], {}))
 print(howSum(7, [2,3,5,7], {}))
