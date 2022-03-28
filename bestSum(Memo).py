8aeb744f9bdbf14d055db5f375bc3aa5444f8b01
Mohan22 <81840891+MohanSrinivas22@users.noreply.github.com>
2022-03-28 22:13:32 +0530

    Create bestSum(Memo).py

---
+++ bestSum(Memo).py
@@ -0,0 +1,23 @@
+def bestSum(target, nums, memo):
+    if target in memo : return memo[target]
+    if target == 0 : return []
+    if target < 0 : return None
+    best=None
+    for num in nums:
+        rem = target - num
+        res = bestSum(rem, nums, memo)
+        if res != None:
+            temp = res + [num]
+            memo[target] = temp[:]
+            if best == None or len(temp) < len(best):
+                best = temp[:]
+    return best
+
+print(bestSum(7, [5,3,4,7], {}))
+print(bestSum(8, [1,4,5], {}))
+print(bestSum(100, [1,2,5,25], {}))
+
+# Sample output :-
+'''[7]
+[4, 4]
+[25, 25, 25, 25]'''
