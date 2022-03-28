489a0583f2e0755281ef369a9417fe6f7d723d44
Mohan22 <81840891+MohanSrinivas22@users.noreply.github.com>
2022-03-28 21:43:27 +0530

    Create howSum(Memo).py

---
+++ howSum(Memo).py
@@ -0,0 +1,19 @@
+def howSum(targetSum, nums, memo):
+    if targetSum in memo : return memo[targetSum]
+    if targetSum == 0 : return []
+    if targetSum < 0 : return None
+    for num in nums:
+        rem = targetSum - num
+        res = howSum(rem, nums, memo)
+        if res != None : 
+            memo[targetSum] = res + [num]
+            return memo[targetSum]
+    return None
+
+
+print(howSum(7, [2], {}))
+print(howSum(7, [2,3,5,7], {}))
+
+# Sample output :-
+'''None
+[3, 2, 2]'''
