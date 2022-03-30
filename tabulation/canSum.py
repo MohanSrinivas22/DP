07712a9dd8a3e150a17654a21f73fb0357ff5816
Mohan22 <81840891+MohanSrinivas22@users.noreply.github.com>
2022-03-30 19:56:26 +0530

    returns boolean value of whether the target sum is possible with given array

---
+++ tabulation/canSum.py
@@ -0,0 +1,12 @@
+def canSum(target, nums):
+    ans=[False]*(target+1)
+    ans[0]=True
+    for val in range(1,target+1):
+        for num in nums:
+            rem=val-num
+            if rem>=0 and ans[rem]!=False:ans[val]=ans[rem]
+    # print(ans)
+    return ans[target]
+
+print(canSum(7, [5,3,4,7])) # True
+print(canSum(300, [7,14]))  # False
