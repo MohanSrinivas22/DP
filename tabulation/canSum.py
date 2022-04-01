819079cc894ff0ed44291c5dbcd065ecc5c700a5
Mohan22 <81840891+MohanSrinivas22@users.noreply.github.com>
2022-04-01 07:41:09 +0530

    Update canSum.py

--- tabulation/canSum.py
+++ tabulation/canSum.py
@@ -4,7 +4,7 @@ def canSum(target, nums):
     for val in range(1,target+1):
         for num in nums:
             rem=val-num
-            if rem>=0 and ans[rem]!=False:ans[val]=ans[rem]
+            if rem>=0 and ans[rem]:ans[val]=ans[rem];break
     # print(ans)
     return ans[target]
 
