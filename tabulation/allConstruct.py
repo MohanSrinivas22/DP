e0f43be623491fdb21bbea5961ce7be05ff30f88
Mohan22 <81840891+MohanSrinivas22@users.noreply.github.com>
2022-04-01 20:55:00 +0530

    Create allConstruct.py

---
+++ tabulation/allConstruct.py
@@ -0,0 +1,22 @@
+def allConstruct(target, words):
+    n=len(target)+1
+    ans=[[] for _ in range(n)]
+    ans[0]=[[]]
+    for val in range(n):
+        for word in words:
+            m=val+len(word)    
+            if target[val:m]==word and ans[val]!=[]:
+                for j in ans[val]:
+                    temp=j+[word]
+                    ans[m].append(temp)
+    # print(ans)
+    return ans[n-1]
+                
+    print(ans)
+    return ans[n-1]
+
+print(allConstruct('abcdef', ['ab','abc','abcd','def','cd'])) 
+print(allConstruct('purple', ['purpl','purp','ur','le','p']))
+
+# [['abc', 'def']]
+# [['purp', 'le'], ['p', 'ur', 'p', 'le']]
